9627cde3dad69e100cd2c33cdc691bf7645edd79
xseignard <xavier.seignard@gmail.com>
2018-12-10 12:26:07 +0100

    OR NOT AND

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -128,7 +128,7 @@ void ofApp::updateSerial() {
 
 //--------------------------------------------------------------
 void ofApp::update() {
-	if (step != "win" && step != "password") updateSerial();
+	if (step != "win" || step != "password") updateSerial();
 	if (step == "password") uPassword();
 	else if (step == "passwordFail") uPasswordFail();
 	else if (step == "scan") uScan();
